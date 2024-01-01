e79666e918f37d26011111e3f2a5bfdc88ba3e18
Moud Mpofu <mpofunkazi@gmail.com>
2024-01-01 15:07:52 +0100

    added forecast structure

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,22 +1,27 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-import "./App.css";
+import "./forecast.css";
 
 export default function Forecast() {
   return (
     <div className="forecast">
-      <p>
-        <em>
-          Clear conditions from 00:00 - 03:00 with partly cloudy conditions
-          expected at 03:00
-        </em>
-      </p>
       <div className="header">
         <em>📅 Daily Forecast</em>
       </div>
       <div className="dailyForecast" id="dailyforecast"></div>
 
       <hr />
+      <div className="row">
+        <div className="col">
+          <div className="forecastDay">Thu</div>
+          <div className="icon">Icon</div>
+          <div className="forecastTemp">
+            <span className="temp-min">12</span>{" "}
+            <span className="tem-max">20 ℃</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
